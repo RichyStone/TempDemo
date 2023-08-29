06d54c20df17268f508a0a57712a0b4bdc88b934
shijiafu <563867771@qq.com>
2023-08-29 17:29:42 +0800

    ExampleForConverter

---
+++ WPFCore/Convert/int2stringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfCore.Convert
+{
+    public class Int2stringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var obj = value.ToString();
+            var val = int.TryParse(obj, out _) ? int.Parse(obj) : 0;
+            return val;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
