4724f4e1c046d4fa51a5b65db6bb002461827206
shijiafu <563867771@qq.com>
2023-08-29 14:56:22 +0800

    CommunityToolkit练习

---
+++ WPFCore/ViewModel/LoginViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCore.ViewModel
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        public LoginViewModel()
+        {
+
+        }
+
+        [ObservableProperty]
+        private string? userName;
+
+        [ObservableProperty]
+        private string? password;
+
+        partial void OnUserNameChanged(string? oldValue, string? newValue)
+        {
+            if (string.IsNullOrWhiteSpace(newValue))
+                UserName = oldValue;
+            else
+                UserName = newValue;
+        }
+
+        partial void OnPasswordChanged(string? oldValue, string? newValue)
+        {
+
+            if (string.IsNullOrWhiteSpace(newValue))
+                Password = oldValue;
+            else
+                Password = newValue;
+        }
+
+        [RelayCommand]
+        private void LoginFunc()
+        {
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password))
+            {
+                MessageBox.Show("用户名或密码为空！", "登录", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+        }
+
+
+    }
+}
