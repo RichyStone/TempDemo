53841f484c2d7d0a4247b09fbdb94e65830afd45
shijiafu <jiafu.shi@semight.com>
2023-09-20 17:27:45 +0800

    feature：实现一个ViewModelLocator

--- WpfFrameWork/ViewModel/MainViewModel.cs
+++ WpfFrameWork/ViewModel/MainViewModel.cs
@@ -7,9 +7,15 @@ using System.Threading.Tasks;
 
 namespace WpfFrameWork.ViewModel
 {
-    public class MainViewModel:ObservableObject
+    public class MainViewModel : ObservableObject
     {
+        private string name;
 
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
 
     }
 }
