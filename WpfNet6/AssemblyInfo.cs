e9490bc622f2fa988ef07dac4fd96e067bde0fa8
shijiafu <jiafu.shi@semight.com>
2023-09-22 17:38:33 +0800

    feature：命名空间别名

--- WpfNet6/AssemblyInfo.cs
+++ WpfNet6/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -8,3 +9,8 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: XmlnsDefinition("http://CustomUI/ResourceToolKit/2023/xaml", "WpfNet6.CommonUi")]
+[assembly: XmlnsDefinition("http://CustomUI/ResourceToolKit/2023/xaml", "WpfNet6.CommonResource")]
+[assembly: XmlnsDefinition("http://CustomUI/ResourceToolKit/2023/xaml", "WpfNet6.Converters")]
+[assembly: XmlnsPrefix("http://CustomUI/ResourceToolKit/2023/xaml", "tool")]
