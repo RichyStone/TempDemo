e250d1667dc84d88c7735917954752babe6b6caa
shijiafu <jiafu.shi@semight.com>
2023-11-01 18:43:57 +0800

    Feature：添加超时判断扩展方法

---
+++ Common/CommonTools/ExtensionMethods/ExtensionAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.CommonTools.ExtensionMethods
+{
+    public static class ExtensionAdapter
+    {
+        public static bool JudgeTimeOut(this Task task, int TimeOout)
+        {
+            var completed = Task.WhenAny(task, Task.Delay(TimeOout));
+            return completed == task;
+        }
+
+        public static bool JudgeTimeOut(this Task task, TimeSpan TimeOout)
+        {
+            var completed = Task.WhenAny(task, Task.Delay(TimeOout));
+            return completed == task;
+        }
+    }
+}
